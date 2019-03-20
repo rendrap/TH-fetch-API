2bb2f254c20a389ea52dfc62f8840bf3fabf9d45
rendrap <rendrapradana@gmail.com>
2019-03-20 22:23:04 +0700

    basic fetch function

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-
-
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(data => console.log(data));
 
 
 // ------------------------------------------
