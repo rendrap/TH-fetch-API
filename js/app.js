0945d7bde31b4b678e9bd972df8198045c43e03b
rendrap <rendrapradana@gmail.com>
2019-03-21 11:20:13 +0700

    add fetchData function as generic fetch method

--- js/app.js
+++ js/app.js
@@ -4,14 +4,31 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
+const fetchData = url => {
+    return fetch(url)
+    .then(res => res.json())
+};
+
+fetchData('https://dog.ceo/api/breeds/list')
+  .then(data => {
+    // console.log(data.message);
+    generateOptions(data.message);
+  });
+
+fetchData('https://dog.ceo/api/breeds/image/random')
   .then(data => generateImage(data.message));
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function generateOptions(data) {
+    const options = data.map(item =>
+        `<option value=${item}>${item}</option>`
+        ).join('');
+    select.innerHTML = options;
+}
+
 function generateImage(data) {
   const html = `
     <img src=${data} alt>
